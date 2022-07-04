d42d0f20cdb0dc58367d82cc6b2242411c42e4e0
palakaggarwal <palak@sopeonow.com>
2022-07-04 20:02:33 +0530

    post request

--- poll/views.py
+++ poll/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from datetime import datetime
 from .models import Fruit
 
@@ -14,9 +14,35 @@ def home(request):
 
 
 def page1(request):
-    # select * from fruit
+    if request.method=='POST':
+        # print(request.POST)
+        kw = {
+            'name':request.POST.get('name'),
+            'color':request.POST.get('color'),
+            'taste':request.POST.get('taste'),
+            'price':request.POST.get('price'),
+            'quantity':request.POST.get('quantity'),
+        }
+        f = Fruit.objects.filter(name__iexact=kw['name'])
+        if not f.exists():
+            f = Fruit.objects.create(**kw)
+            # f.price=100
+            # f.save()
+        return redirect('/page1', permanent=True)
+    elif request.method=='GET':
+        print(request.GET)
+
+    # select * from fruit where color = 'red'
     f = Fruit.objects.all()
+    f1 = Fruit.objects.filter(color__iexact='yellow')
     # print(f.values())
     # print(f.values()[0])
-    context = {'fruits':f.values()}
+    context = {'fruits':f.values(), 'yf':f1.values()}
     return render(request, 'poll/page1.html', context)
+
+'''
+DDL - creation of tables, alter table structure, delete tables
+DML - insertion, deletion, read, update (CRUD) 
+DCL - grant, revoke, roles
+Tcl - commit, rollback, savepoint 
+'''
